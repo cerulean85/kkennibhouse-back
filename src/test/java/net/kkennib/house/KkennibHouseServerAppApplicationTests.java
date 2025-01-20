46a9bb4f4d444feb3659fd0bd0bca81daa0dace8
hh_zhwan85 <zhwan85@hanwha.com>
2025-01-20 22:03:42 +0900

    Update

---
+++ src/test/java/net/kkennib/house/KkennibHouseServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kkennib.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkennibHouseServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
